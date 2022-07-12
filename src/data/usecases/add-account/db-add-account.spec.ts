7a9e9caa299deecba3b0875d95ba29cce395fbec
Leonardo Mendes <lsm.ccomp@gmail.com>
2022-07-12 17:24:26 -0300

    test: ensure DbAddAccount throws if AddAccountReposutory throws

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -91,4 +91,20 @@ describe('DbAddAccount Usecse', () => {
       password: 'hashed_password',
     });
   });
+
+  test('Should throw if AddAccountRepository throws', async () => {
+    const { sut, addAccountRepositoryStub } = makeSut();
+    jest
+      .spyOn(addAccountRepositoryStub, 'add')
+      .mockReturnValueOnce(
+        new Promise((resolve, reject) => reject(new Error())),
+      );
+    const accountData = {
+      name: 'valid_name',
+      email: 'valid_email',
+      password: 'valid_password',
+    };
+    const promise = sut.add(accountData);
+    expect(promise).rejects.toThrow();
+  });
 });
