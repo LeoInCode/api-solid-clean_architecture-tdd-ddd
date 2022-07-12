62788185673ed51ab85b58f626fdaa7bc9a5893a
Leonardo Mendes <lsm.ccomp@gmail.com>
2022-07-12 19:31:59 -0300

    chore: add jest configs for mongodb

---
+++ jest-mongodb-config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  mongodbMemoryServerOptions: {
+    istance: {
+      dbNmae: 'jest',
+    },
+    binary: {
+      version: '4.0.3',
+      skipMD5: true,
+    },
+    autoStart: false,
+  },
+};
--- jest.config.js
+++ jest.config.js
@@ -11,4 +11,5 @@ module.exports = {
   transform: {
     '.+\\.ts$': 'ts-jest',
   },
+  preset: '@shelf/jest-mongodb',
 };
