b77ca60ae71ef577c43b8a711046c1fac49fbb95
Leonardo Mendes <lsm.ccomp@gmail.com>
2022-07-13 15:50:07 -0300

    test: ensure LogControllerDecorator returns the same result of the controller

--- src/main/decorators/log.spec.ts
+++ src/main/decorators/log.spec.ts
@@ -51,4 +51,23 @@ describe('LogController Decorator', () => {
     await sut.handle(httpRequest);
     expect(handleSpy).toHaveBeenCalledWith(httpRequest);
   });
+
+  test('Should return the same result of the controller', async () => {
+    const { sut } = makeSut();
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_mail@mail.com',
+        password: 'any_passowrd',
+        passwordConfirmation: 'any_password',
+      },
+    };
+    const httpResponse = await sut.handle(httpRequest);
+    expect(httpResponse).toEqual({
+      statusCode: 200,
+      body: {
+        name: 'Leo',
+      },
+    });
+  });
 });
