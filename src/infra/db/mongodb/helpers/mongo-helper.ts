556c775822673c132c2a14057fa266bd1e13ff2b
Leonardo Mendes <lsm.ccomp@gmail.com>
2022-07-13 13:14:15 -0300

    refactor: add new validations to MongoHelper

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -3,17 +3,20 @@ import { Collection, MongoClient } from 'mongodb';
 
 export const MongoHelper = {
   client: null as MongoClient,
+  uri: null as string,
 
   async connect(uri: string): Promise<void> {
+    this.uri = uri;
     this.client = await MongoClient.connect(uri);
   },
 
   async disconnect(): Promise<void> {
-    await this.client.close();
+    await this.client?.close();
+    this.client = null;
   },
 
   getCollection(name: string): Collection {
-    return this.client.db().collection(name);
+    return this.client?.db()?.collection(name);
   },
 
   map: (collection: any): any => {
