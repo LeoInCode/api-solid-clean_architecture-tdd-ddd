99a60f76616b6f29ce923e6e4c0bc5da10c7f48f
Leonardo Mendes <lsm.ccomp@gmail.com>
2022-07-14 19:07:16 -0300

    refactor: move data layer interfaces to folders

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -1,6 +1,6 @@
 import { AccountModel } from '../../../../domain/models/account';
 import { AddAccountModel } from '../../../../domain/usecases/add-account';
-import { AddAccountRepository } from '../../../../data/protocols/add-account-respository';
+import { AddAccountRepository } from '../../../../data/protocols/db/add-account-respository';
 import { MongoHelper } from '../helpers/mongo-helper';
 
 export class AccountMongoRepository implements AddAccountRepository {
