7dec4c0da22dd4940166329a0f0b052b9e3003cb
Leonardo Mendes <lsm.ccomp@gmail.com>
2022-07-14 09:07:47 -0300

    feat: ensure LoginController calls EmailValidator with correct wemail

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,8 +1,15 @@
+import { EmailValidator } from '../../protocols/email-validator';
 import { MissingParamError } from '../../errors';
 import { badRequest } from '../../helpers/http-helper';
 import { Controller, HttpRequest, HttpResponse } from '../../protocols';
 
 export class LoginController implements Controller {
+  private readonly emailValidator: EmailValidator;
+
+  constructor(emailValidator: EmailValidator) {
+    this.emailValidator = emailValidator;
+  }
+
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     if (!httpRequest.body.email) {
       return new Promise((resolve) =>
@@ -14,5 +21,7 @@ export class LoginController implements Controller {
         resolve(badRequest(new MissingParamError('password'))),
       );
     }
+
+    this.emailValidator.isValid(httpRequest.body.email);
   }
 }
