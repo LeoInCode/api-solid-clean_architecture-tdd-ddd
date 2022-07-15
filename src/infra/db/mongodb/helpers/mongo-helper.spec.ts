1fcc259d58d91a3ef16fdead2d5e14bea17a2f02
Leonardo Mendes <lsm.ccomp@gmail.com>
2022-07-15 10:09:25 -0300

    test: ensure MongoHelper returns a collection with id instead _id

--- src/infra/db/mongodb/helpers/mongo-helper.spec.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.spec.ts
@@ -5,4 +5,19 @@ describe('Mongo Helper', () => {
     const collectionMapped = MongoHelper.map(null);
     expect(collectionMapped).toBeNull();
   });
+
+  test('Should return a collection with id instead _id', () => {
+    const collectionMapped = MongoHelper.map({
+      _id: 'any_id',
+      name: 'any_name',
+      email: 'any_email',
+      password: 'any_password',
+    });
+    expect(collectionMapped).toEqual({
+      id: 'any_id',
+      name: 'any_name',
+      email: 'any_email',
+      password: 'any_password',
+    });
+  });
 });
