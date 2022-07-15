ac29ae660f6bc859bfebef739294c95ff2858586
Leonardo Mendes <lsm.ccomp@gmail.com>
2022-07-15 02:08:43 -0300

    test: ensure AccountMongoRepository updates the account accessToken on updatedAccessToken success

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -56,4 +56,25 @@ describe('Account Mongo Repository', () => {
     const account = await sut.loadByEmail('any_email@mail.com');
     expect(account).toBeFalsy();
   });
+
+  test('Should update the account accessToken on updateAccessToken success', async () => {
+    const sut = makeSut();
+    const res = await accountCollection.insertOne({
+      name: 'any_name',
+      email: 'any_email@mail.com',
+      password: 'any_password',
+    });
+    let account = await accountCollection.findOne({
+      _id: res.insertedId,
+    });
+    expect(account.accessToken).toBeFalsy();
+
+    await sut.updateAccessToken(res.insertedId.toHexString(), 'any_token');
+    account = await accountCollection.findOne({
+      _id: res.insertedId,
+    });
+
+    expect(account).toBeTruthy();
+    expect(account.accessToken).toBe('any_token');
+  });
 });
