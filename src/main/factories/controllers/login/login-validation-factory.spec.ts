34964088325738702088af8b3fee852649d67d61
Leonardo Mendes <lsm.ccomp@gmail.com>
2022-07-15 12:06:42 -0300

    refactor: separete factories in folders

--- src/main/factories/controllers/login/login-validation-factory.spec.ts
+++ src/main/factories/controllers/login/login-validation-factory.spec.ts
@@ -1,14 +1,13 @@
 import {
-  CompareFieldsValidation,
   EmailValidation,
   RequiredFielValidation,
   ValidationComposite,
-} from '../../../presentation/helpers/validators';
-import { Validation } from '../../../presentation/protocols/validation';
-import { EmailValidator } from '../../../presentation/protocols/email-validator';
-import { makeSignUpValidation } from './signup-validation-factory';
+} from '../../../../presentation/helpers/validators';
+import { Validation } from '../../../../presentation/protocols/validation';
+import { EmailValidator } from '../../../../presentation/protocols/email-validator';
+import { makeLoginValidation } from './login-validation-factory';
 
-jest.mock('../../../presentation/helpers/validators/validation-composite');
+jest.mock('../../../../presentation/helpers/validators/validation-composite');
 
 const makeEmailValdiator = (): EmailValidator => {
   class EmailValidatorStub implements EmailValidator {
@@ -21,14 +20,11 @@ const makeEmailValdiator = (): EmailValidator => {
 
 describe('SignUpValidation Factory', () => {
   test('Should call ValidationComposite with all validations', () => {
-    makeSignUpValidation();
+    makeLoginValidation();
     const validations: Validation[] = [];
-    for (const field of ['name', 'email', 'password', 'passwordConfirmation']) {
+    for (const field of ['email', 'password']) {
       validations.push(new RequiredFielValidation(field));
     }
-    validations.push(
-      new CompareFieldsValidation('password', 'passwordConfirmation'),
-    );
     validations.push(new EmailValidation('email', makeEmailValdiator()));
     expect(ValidationComposite).toHaveBeenCalledWith(validations);
   });
