5d7223404a18f9579b0b68ae01eb02e78ea144da
Leonardo Mendes <lsm.ccomp@gmail.com>
2022-07-16 14:09:34 -0300

    test: ensure AccountMongoRepository will not find an account if find account by token fails

--- src/infra/db/mongodb/account/account-mongo-repository.spec.ts
+++ src/infra/db/mongodb/account/account-mongo-repository.spec.ts
@@ -119,5 +119,11 @@ describe('Account Mongo Repository', () => {
       expect(account.password).toBe('any_password');
       expect(account.password).toBe('any_password');
     });
+
+    test('Should return null if loadByToken fails', async () => {
+      const sut = makeSut();
+      const account = await sut.loadByToken('any_token');
+      expect(account).toBeFalsy();
+    });
   });
 });
