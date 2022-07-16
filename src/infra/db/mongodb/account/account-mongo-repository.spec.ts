d09ef0692f2b05f58d8b3dc61e2b6978a5b35637
Leonardo Mendes <lsm.ccomp@gmail.com>
2022-07-16 17:18:13 -0300

    test: ensure AccountMongoRepository returns null with an invalid role

--- src/infra/db/mongodb/account/account-mongo-repository.spec.ts
+++ src/infra/db/mongodb/account/account-mongo-repository.spec.ts
@@ -102,7 +102,25 @@ describe('Account Mongo Repository', () => {
       expect(account.password).toBe('any_password');
     });
 
-    test('Should return an account on loadByToken with role', async () => {
+    test('Should return an account on loadByToken with admin role', async () => {
+      const sut = makeSut();
+      await accountCollection.insertOne({
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        accessToken: 'any_token',
+        role: 'admin',
+      });
+      const account = await sut.loadByToken('any_token', 'admin');
+      expect(account).toBeTruthy();
+      expect(account.id).toBeTruthy();
+      expect(account.name).toBe('any_name');
+      expect(account.email).toBe('any_email@mail.com');
+      expect(account.password).toBe('any_password');
+      expect(account.password).toBe('any_password');
+    });
+
+    test('Should return null on loadByToken with invalid role', async () => {
       const sut = makeSut();
       await accountCollection.insertOne({
         name: 'any_name',
@@ -111,13 +129,8 @@ describe('Account Mongo Repository', () => {
         accessToken: 'any_token',
         role: 'any_role',
       });
-      const account = await sut.loadByToken('any_token', 'any_role');
-      expect(account).toBeTruthy();
-      expect(account.id).toBeTruthy();
-      expect(account.name).toBe('any_name');
-      expect(account.email).toBe('any_email@mail.com');
-      expect(account.password).toBe('any_password');
-      expect(account.password).toBe('any_password');
+      const account = await sut.loadByToken('any_token', 'admin');
+      expect(account).toBeFalsy();
     });
 
     test('Should return null if loadByToken fails', async () => {
