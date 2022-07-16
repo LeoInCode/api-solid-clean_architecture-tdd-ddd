536cf1af9fd7c403bd87b6cb648f0f47358a9900
Leonardo Mendes <lsm.ccomp@gmail.com>
2022-07-16 19:45:33 -0300

    test: ensure SurveyMongoRepository load all surveys

--- src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
@@ -44,4 +44,37 @@ describe('Account Mongo Repository', () => {
       expect(survey).toBeTruthy();
     });
   });
+
+  describe('loadAll()', () => {
+    test('Should load all surveys on succes', async () => {
+      const sut = makeSut();
+      await surveyCollection.insertMany([
+        {
+          question: 'any_question',
+          answers: [
+            {
+              image: 'any_image',
+              answer: 'any_answer',
+            },
+          ],
+          date: new Date(),
+        },
+        {
+          question: 'other_question',
+          answers: [
+            {
+              image: 'other_image',
+              answer: 'other_answer',
+            },
+          ],
+          date: new Date(),
+        },
+      ]);
+      const surveys = await sut.loadAll();
+      expect(surveys).toBeInstanceOf(Array);
+      expect(surveys.length).toBe(2);
+      expect(surveys[0].question).toBe('any_question');
+      expect(surveys[1].question).toBe('other_question');
+    });
+  });
 });
