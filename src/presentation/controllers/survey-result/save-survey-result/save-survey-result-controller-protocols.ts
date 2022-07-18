a045c97583678b0fe80516bb0884e148db7cec28
Leonardo Mendes <lsm.ccomp@gmail.com>
2022-07-18 20:06:21 -0300

    feat: ensure SaveSurveyResultController calls SaveSurveyResult with correct values

--- src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller-protocols.ts
+++ src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller-protocols.ts
@@ -2,3 +2,4 @@ export * from '@/presentation/protocols';
 export * from '@/presentation/protocols/controller';
 export * from '@/domain/usecases/survey/load-survey-by-id';
 export * from '@/domain/models/survey';
+export * from '@/domain/usecases/survey-result/save-survey-result';
--- src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.ts
+++ src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.ts
@@ -8,21 +8,32 @@ import {
   HttpResponse,
   Controller,
   LoadSurveyById,
+  SaveSurveyResult,
 } from './save-survey-result-controller-protocols';
 
 export class SaveSarveyResultController implements Controller {
-  constructor(private readonly laodSurveyById: LoadSurveyById) {}
+  constructor(
+    private readonly loadSurveyById: LoadSurveyById,
+    private readonly saveSurveyResult: SaveSurveyResult,
+  ) {}
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
       const { surveyId } = httpRequest.params;
       const { answer } = httpRequest.body;
-      const survey = await this.laodSurveyById.loadById(surveyId);
+      const { accountId } = httpRequest;
+      const survey = await this.loadSurveyById.loadById(surveyId);
       if (survey) {
         const answers = survey.answers.map((a) => a.answer);
         if (!answers.includes(answer)) {
           return forbidden(new InvalidParamError('answer'));
         }
+        await this.saveSurveyResult.save({
+          accountId,
+          surveyId,
+          answer,
+          date: new Date(),
+        });
       } else {
         return forbidden(new InvalidParamError('surveyId'));
       }
