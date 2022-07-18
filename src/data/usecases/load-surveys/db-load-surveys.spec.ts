2cf542d7c991ed6b3a05b1eff1073468e3bd27be
Leonardo Mendes <lsm.ccomp@gmail.com>
2022-07-17 21:00:14 -0300

    refactor: aadd data usecases protocol files to simplify imports

--- src/data/usecases/load-surveys/db-load-surveys.spec.ts
+++ src/data/usecases/load-surveys/db-load-surveys.spec.ts
@@ -1,7 +1,9 @@
 import MockDate from 'mockdate';
 import { DbLoadSurveys } from './db-load-surveys';
-import { LoadSurveysRepository } from '@/data/protocols/db/survey/load-surveys-repository';
-import { SurveyModel } from '@/domain/models/survey';
+import {
+  LoadSurveysRepository,
+  SurveyModel,
+} from './db-load-surveys-protocols';
 
 const makeFakeSurveys = (): SurveyModel[] => {
   return [
