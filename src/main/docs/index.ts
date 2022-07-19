12a95ba78f3f50ee6e47dc14f8c6c67ccb2f6f6d
Leonardo Mendes <lsm.ccomp@gmail.com>
2022-07-19 18:26:19 -0300

    feat: add swagger api

---
+++ src/main/docs/index.ts
@@ -0,0 +1,8 @@
+export default {
+  openapi: '3.0.0',
+  info: {
+    title: 'Solid Clean Architecture TDD DDD',
+    description: 'Projeto desenvolvido seguindo o curso do Rodrigo Mango',
+    version: '1.0.0',
+  },
+};
