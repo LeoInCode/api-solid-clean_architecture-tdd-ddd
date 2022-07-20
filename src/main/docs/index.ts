a271c185cba9a3e384dafb8290dd6cbff14d63fc
Leonardo Mendes <lsm.ccomp@gmail.com>
2022-07-19 23:17:33 -0300

    docs: add login api docs

--- src/main/docs/index.ts
+++ src/main/docs/index.ts
@@ -1,8 +1,36 @@
+import { unauthorized, serverError, badRequest, notFound } from './components';
+import { loginPath } from './paths';
+import { accountSchema, loginParamsSchema, errorSchema } from './schemas';
+
 export default {
   openapi: '3.0.0',
   info: {
     title: 'Solid Clean Architecture TDD DDD',
     description: 'Projeto desenvolvido seguindo o curso do Rodrigo Mango',
     version: '1.0.0',
+  },
+  servers: [
+    {
+      url: '/api',
+    },
+  ],
+  tags: [
+    {
+      name: 'Login',
+    },
+  ],
+  paths: {
+    '/login': loginPath,
+  },
+  schemas: {
+    account: accountSchema,
+    loginParams: loginParamsSchema,
+    error: errorSchema,
+  },
+  components: {
+    badRequest,
+    serverError,
+    unauthorized,
+    notFound,
   },
 };
