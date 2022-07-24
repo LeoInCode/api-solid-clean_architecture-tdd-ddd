4154230babd3de390edf5ecfcc0bb1f541b710bd
Leonardo Mendes <lsm.ccomp@gmail.com>
2022-07-24 13:03:04 -0300

    refactor: rename folder jswt-adapter to jwt-adapter

--- src/main/factories/usecases/account/authentication/db-authentication-factory.ts
+++ src/main/factories/usecases/account/authentication/db-authentication-factory.ts
@@ -1,7 +1,7 @@
 import { Authentication } from '@/domain/usecases/account/authentication';
 import { DbAuthentication } from '@/data/usecases/account/authentication/db-authentication';
 import { BcryptAdapter } from '@/infra/criptography/bcrypt-adapter/bcrypt-adapter';
-import { JwtAdapter } from '@/infra/criptography/jswt-adapter/jwt-adapter';
+import { JwtAdapter } from '@/infra/criptography/jwt-adapter/jwt-adapter';
 import { AccountMongoRepository } from '@/infra/db/mongodb/account/account-mongo-repository';
 import env from '@/main/config/env';
 
