5b251677a502b75d1c9b8fbcd20b3144bcda858a
Leonardo Mendes <lsm.ccomp@gmail.com>
2022-08-11 01:22:53 -0300

    refactor: move survey result response to own method

--- src/data/usecases/db-load-survey-result.ts
+++ src/data/usecases/db-load-survey-result.ts
@@ -1,5 +1,5 @@
 import { LoadSurveyResultRepository, LoadSurveyByIdRepository } from '@/data/protocols';
-import { SurveyResultModel } from '@/domain/models';
+import { SurveyModel, SurveyResultModel } from '@/domain/models';
 import { LoadSurveyResult } from '@/domain/usecases';
 
 export class DbLoadSurveyResult implements LoadSurveyResult {
@@ -12,19 +12,23 @@ export class DbLoadSurveyResult implements LoadSurveyResult {
     let surveyResult = await this.loadSurveyResultRepository.loadBySurveyId(surveyId, accountId);
     if (!surveyResult) {
       const survey = await this.loadSurveyByIdRepositoryStub.loadById(surveyId);
-      surveyResult = {
-        surveyId: survey.id,
-        question: survey.question,
-        date: survey.date,
-        answers: survey.answers.map((answer) =>
-          Object.assign(answer, {
-            count: 0,
-            percent: 0,
-            isCurrentAccountAnswer: false,
-          }),
-        ),
-      };
+      surveyResult = this.makeEmptyResult(survey);
     }
     return surveyResult;
+  }
+
+  private makeEmptyResult(survey: SurveyModel): SurveyResultModel {
+    return {
+      surveyId: survey.id,
+      question: survey.question,
+      date: survey.date,
+      answers: survey.answers.map((answer) =>
+        Object.assign(answer, {
+          count: 0,
+          percent: 0,
+          isCurrentAccountAnswer: false,
+        }),
+      ),
+    };
   }
 }
