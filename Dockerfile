eb2c6e91638682780413bb43c660d1f908d3fbcc
Leonardo Mendes <lsm.ccomp@gmail.com>
2022-08-14 22:06:19 -0300

    chore: enable debug with vscode and remove dockerfile to use docker-compose

--- Dockerfile
+++
@@ -1,4 +0,0 @@
-FROM node:14
-WORKDIR /usr/src/api-solid-clean_architecture-tdd-ddd
-COPY ./package.json .
-RUN npm install --only=prod
--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
-  mongoUrl: process.env.MONGO_URL || 'mongodb://mongo:27017/api-solid-clean_architecture-tdd-ddd',
+  mongoUrl: process.env.MONGO_URL || 'mongodb://127.0.0.1:27017/api-solid-clean_architecture-tdd-ddd',
   port: process.env.PORT || 5050,
   jwtSecret: process.env.JWT_SECRET || 'DKJ8923@#23SUDHJ==',
 };
