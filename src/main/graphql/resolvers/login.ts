03ba175fc0357657283b3ed5bf9fdf0748ba1076
Leonardo Mendes <lsm.ccomp@gmail.com>
2022-08-21 17:12:17 -0300

    feat: make login resolver use login controller

--- src/main/graphql/resolvers/login.ts
+++ src/main/graphql/resolvers/login.ts
@@ -1,10 +1,8 @@
+import { adaptResolver } from '@/main/adapters';
+import { makeLoginController } from '../../factories/controllers/login-controller-factory';
+
 export default {
   Query: {
-    login() {
-      return {
-        accessToken: 'any_token',
-        name: 'any_name',
-      };
-    },
+    login: async (parent: any, args: any) => adaptResolver(makeLoginController(), args),
   },
 };
