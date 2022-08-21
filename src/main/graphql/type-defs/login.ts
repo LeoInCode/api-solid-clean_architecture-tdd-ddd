1750b77dea5d19142dc9a7f5bc3d6c9aa5c5bc55
Leonardo Mendes <lsm.ccomp@gmail.com>
2022-08-21 19:07:55 -0300

    feat: add signUp mutation

--- src/main/graphql/type-defs/login.ts
+++ src/main/graphql/type-defs/login.ts
@@ -5,6 +5,10 @@ export default gql`
     login(email: String!, password: String!): Account!
   }
 
+  extend type Mutation {
+    signUp(email: String!, password: String!, password: String!, passwordConfirmation: String!): Account!
+  }
+
   type Account {
     accessToken: String!
     name: String!
