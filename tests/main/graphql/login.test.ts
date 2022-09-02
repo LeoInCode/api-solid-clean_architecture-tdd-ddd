3a96787a30ae71ca53f458c28071fffc5cb07be9
Leonardo Mendes <lsm.ccomp@gmail.com>
2022-09-02 01:11:16 -0300

    test: ensure Login Query returns an account on valid credentials

---
+++ tests/main/graphql/login.test.ts
@@ -0,0 +1,47 @@
+import request from 'supertest';
+import { Collection } from 'mongodb';
+import { hash } from 'bcrypt';
+import { Express } from 'express';
+import { setupApp } from '@/main/config/app';
+import { MongoHelper } from '@/infra/db';
+
+let accountCollection: Collection;
+let app: Express;
+
+describe('Login GraphQL', () => {
+  beforeAll(async () => {
+    app = await setupApp();
+    await MongoHelper.connect(process.env.MONGO_URL);
+  });
+
+  afterAll(async () => {
+    await MongoHelper.disconnect();
+  });
+
+  beforeEach(async () => {
+    accountCollection = MongoHelper.getCollection('accounts');
+    await accountCollection.deleteMany({});
+  });
+
+  describe('Login Query', () => {
+    const query = `query {
+      login(email: "leo@mail.com", password: "123") {
+        accessToken
+        name
+      }
+    }`;
+
+    test('Should return an Account on valid credentials', async () => {
+      const password = await hash('123', 12);
+      await accountCollection.insertOne({
+        name: 'Leo',
+        email: 'leo@mail.com',
+        password,
+      });
+      const res = await request(app).post('/graphql').send({ query });
+      expect(res.status).toBe(200);
+      expect(res.body.data.login.accessToken).toBeTruthy();
+      expect(res.body.data.login.name).toBe('Leo');
+    });
+  });
+});
